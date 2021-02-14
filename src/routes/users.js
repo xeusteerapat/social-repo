4c9656bc04517e5fd03a7df21db7127556e22b5b
xeusteerapat <xeus085@gmail.com>
2021-02-14 16:16:53 +0700

    [EDIT] complete CRUD

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,15 +1,57 @@
 const express = require('express');
+const UserRepo = require('../repositories/user-repo');
 
 const router = express.Router();
 
-router.get('/users', async (req, res) => {});
+router.get('/users', async (req, res) => {
+  const users = await UserRepo.find();
 
-router.get('/users/:id', async (req, res) => {});
+  res.send(users);
+});
 
-router.post('/users', async (req, res) => {});
+router.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
 
-router.put('/users/:id', async (req, res) => {});
+  const user = await UserRepo.findById(id);
 
-router.delete('/users/:id', async (req, res) => {});
+  if (user) {
+    res.send(user);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.post('/users', async (req, res) => {
+  const { bio, username } = req.body;
+
+  const user = await UserRepo.insert(username, bio);
+
+  res.status(201).send(user);
+});
+
+router.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const { username, bio } = req.body;
+
+  const user = await UserRepo.update(id, username, bio);
+
+  if (user) {
+    res.send(user);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.delete('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const user = await UserRepo.delete(id);
+
+  if (user) {
+    res.send(user);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 module.exports = router;
