924a045c84e267283830b4e0d589a43c25fcea29
xeusteerapat <xeus085@gmail.com>
2021-02-14 14:32:10 +0700

    [EDIT] setup pg pool connection

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('./src/app');
+const pool = require('./src/pool');
+require('dotenv').config();
+
+pool
+  .connect({
+    host: 'localhost',
+    port: 5432,
+    database: 'socialnetwork',
+    users: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+  })
+  .then(() => {
+    app().listen(3005, () => {
+      console.log('server running 3005 🚀');
+    });
+  })
+  .catch(err => console.error(err));
---
+++ src/pool.js
@@ -0,0 +1,12 @@
+const pg = require('pg');
+
+class Pool {
+  _pool = null;
+
+  connect(options) {
+    this._pool = new pg.Pool(options);
+    return this._pool.query('SELECT 1 + 1');
+  }
+}
+
+module.exports = new Pool();
